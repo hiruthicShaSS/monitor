3b9db407da455c66207d307dad63381fd777fd72
hiruthic2002 <predatorsha2002@gmail.com>
2020-12-06 21:16:26 +0530

    still fuck me

---
+++ monitor/lib/chart.dart
@@ -0,0 +1,62 @@
+import "package:flutter/material.dart";
+import 'package:pie_chart/pie_chart.dart';
+
+class Plotter extends StatefulWidget {
+  List apps;
+  List timeSpent;
+  Plotter({Key key, @required this.apps, @required this.timeSpent})
+      : super(key: key);
+
+  @override
+  _PlotterState createState() => _PlotterState();
+}
+
+class _PlotterState extends State<Plotter> {
+  @override
+  Widget build(BuildContext context) {
+    Map<String, double> dataMap = new Map();
+    for (int i = 0; i < widget.apps.length; i++) {
+      dataMap[widget.apps[i]] = widget.timeSpent[i];
+    }
+    print(dataMap);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Grpah"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(1.0),
+          child: Container(
+            alignment: Alignment.center,
+            child: PieChart(
+              dataMap: dataMap,
+              animationDuration: Duration(seconds: 1),
+              chartLegendSpacing: 32,
+              chartRadius: MediaQuery.of(context).size.width / 1.2,
+              // colorList: [],
+              initialAngleInDegree: 0,
+              chartType: ChartType.ring,
+              ringStrokeWidth: 32,
+              centerText: "App Usage",
+              legendOptions: LegendOptions(
+                showLegendsInRow: false,
+                legendPosition: LegendPosition.bottom,
+                showLegends: true,
+                legendTextStyle:
+                    TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              chartValuesOptions: ChartValuesOptions(
+                showChartValueBackground: true,
+                showChartValues: true,
+                showChartValuesInPercentage: false,
+                showChartValuesOutside: false,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ monitor/lib/main.dart
@@ -0,0 +1,253 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:monitor/chart.dart';
+import 'package:device_info/device_info.dart';
+import 'package:crypto/crypto.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          colorScheme: ColorScheme.dark()),
+      home: MyHomePage(title: 'Monitor'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const String URL = "https://toxic-cat.herokuapp.com/";
+  Map data;
+  double timeSpentOnWork = 0.0, timeSpentOnFun = 0.0;
+
+  List<String> apps = new List();
+  List<String> category = new List();
+  List<double> timeSpent = new List();
+  List<String> creationTime = new List();
+  Map<String, String> icons = new Map();
+
+  Widget _status = Text("Fetching...");
+
+  Future<Map> getData() async {
+    var response = await http.get('${URL}download?file=tracking.json&pass=sha');
+    final Map data = jsonDecode(response.body);
+    // print(data);
+    return data;
+  }
+
+  void setUp() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    String device =
+        "${androidInfo.androidId} ${androidInfo.board} ${androidInfo.brand} ${androidInfo.device} ${androidInfo.display} ${androidInfo.hardware} ${androidInfo.manufacturer} ${androidInfo.model} ${androidInfo.product}";
+    var deviceBytes = utf8.encode(device);
+    var deviceDigest = sha512.convert(deviceBytes);
+    print(androidInfo.androidId);
+    if (deviceDigest.toString() !=
+        "3b101badaa76a5a019478f03ba711ebb9ece838b73698a3983f857c0019e68de3b90aa82207cd1a1dd91752396df6824b5d5deca9bb9bb736fe8d524d1a567dd") {
+      exit(-1);
+    }
+
+    setState(() {
+      _status = Text("Fetching...");
+    });
+
+    List<Widget> _buildChildren() {
+      // Clear the existing array
+      apps.clear();
+      timeSpent.clear();
+      creationTime.clear();
+      icons.clear();
+
+      // Populate the array with new data from server
+      data["data"].forEach((key, value) {
+        if (key != "lastUpdated") {
+          apps.add(value["appName"]);
+          category.add(value["category"]);
+          timeSpent.add(value["timeSpentOnWork"]);
+          creationTime.add(value["creationTime"]);
+          icons[value["appName"]] = value["appIcon"];
+        }
+      });
+      
+      int work = 0, fun = 0;
+      for (int i=0; i<apps.length; i++) {
+        if (category[i] == "Work") {
+          work++;
+          timeSpentOnWork += timeSpent[i];
+        } else {
+          fun++;
+          timeSpentOnFun += timeSpent[i];
+        }
+      }
+      timeSpentOnWork /= work;
+      timeSpentOnFun /= fun;
+
+      return new List<Widget>.generate(apps.length, (index) {
+        return InkWell(
+          child: Card(
+            color: (category[index] == "Work")
+                ? Colors.green[900]
+                : Colors.blueAccent[700],
+            child: ListTile(
+              leading: Image.network(icons[apps[index]]),
+              title: Container(
+                child: Row(
+                  children: [
+                    Text(apps[index]),
+                    Spacer(),
+                    Column(
+                      children: [
+                        Text(
+                          "${timeSpent[index].toInt()} min's",
+                          style: TextStyle(color: Colors.amberAccent),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Text(
+                            "Created @ ${creationTime[index]}",
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          onTap: () {
+            print(creationTime[index]);
+          },
+        );
+      });
+    }
+
+    data = await getData();
+    if (data["code"] == 404) {
+      setState(() {
+        _status = Text(data["message"]);
+      });
+
+      // Scaffold.of(context).showSnackBar(SnackBar(
+      //   content: Text(data["message"]),
+      // ));
+    } else {
+      setState(() {
+        _status = Text("Fetching...");
+        _status = Column(
+          children: _buildChildren(),
+        );
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.replay_outlined),
+            color: Colors.red[400],
+            onPressed: () => setUp(),
+          ),
+        ],
+        backgroundColor: Colors.black54,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.red,
+            height: MediaQuery.of(context).size.height / 10,
+            width: MediaQuery.of(context).size.width / 0.2,
+            child: Center(
+                child: Column(
+              children: [
+                Text(
+                  "Time spent on Fun: $timeSpentOnFun min's",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 25.0,
+                  ),
+                ),
+                Text(
+                  "Time spent on Work: $timeSpentOnWork min's",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 25.0,
+                  ),
+                ),
+                Text("Last updated: ${data["data"]["lastUpdated"]}"),
+              ],
+            )),
+          ),
+          SizedBox(height: 10.0),
+          Divider(thickness: 3.0, color: Colors.black,),
+          SizedBox(height: 10.0),
+          Center(
+            child: Scrollbar(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    _status,
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    Plotter(apps: apps, timeSpent: timeSpent)),
+          );
+        },
+        tooltip: 'Plot graph',
+        child: Icon(Icons.poll_outlined),
+        splashColor: Colors.black,
+      ),
+    );
+  }
+}
